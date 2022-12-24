375c80fa12e398cd9b85a0da5ad4737d8d4cf0d1
fedecalla <fedecalla974@gmail.com>
2022-12-24 12:49:33 -0300

    Creacion de los archivos

---
+++ integracionJava.js
@@ -0,0 +1,6 @@
+console.log('Bienvenidos al trabajo de integracion numero 1')
+let uno = 1
+let halago = 'FEDE ES MUY CAPO'
+
+console.log(`Para insertar valores dentro de una string de la siguiente manera: ${halago} , solo es necesario
+insertar un simbolo $ seguido de dos llaves y dentro colocar el valor de la variable que queramos imprimir`)
